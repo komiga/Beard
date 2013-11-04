21e39ddb8f495599bc82f5f7cfbb80c6f4641cda
Coranna Howard <me@komiga.com>
2013-11-04 01:48:30 -0500

    tty: added this_path().

---
+++ include/Beard/tty/Ops.hpp
@@ -0,0 +1,37 @@
+/**
+@file tty/Ops.hpp
+@brief %Terminal operations.
+
+@author Tim Howard
+@copyright 2013 Tim Howard under the MIT license;
+see @ref index or the accompanying LICENSE file for full text.
+*/
+
+#ifndef BEARD_TTY_OPS_HPP_
+#define BEARD_TTY_OPS_HPP_
+
+#include <Beard/config.hpp>
+#include <Beard/String.hpp>
+
+namespace Beard {
+namespace tty {
+
+// Forward declarations
+
+/**
+	@addtogroup tty
+	@{
+*/
+
+/**
+	Get the pathname to the process's controlling terminal.
+*/
+String
+this_path() noexcept;
+
+/** @} */ // end of doc-group tty
+
+} // namespace tty
+} // namespace Beard
+
+#endif // BEARD_TTY_OPS_HPP_
---
+++ src/Beard/tty/Ops.cpp
@@ -0,0 +1,19 @@
+
+#include <Beard/tty/Ops.hpp>
+
+#include <cstring>
+#include <stdio.h>
+
+namespace Beard {
+namespace tty {
+
+String
+this_path() noexcept {
+	char buf[L_ctermid];
+	buf[L_ctermid - 1u] = '\0';
+	::ctermid(buf);
+	return String{buf};
+}
+
+} // namespace tty
+} // namespace Beard
