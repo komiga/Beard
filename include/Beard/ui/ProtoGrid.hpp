7f9c078eafbfbc66642c8a0c9ca71e4299849606
Coranna Howard <me@komiga.com>
2014-02-14 22:49:45 -0500

    ui: added ProtoGrid widget base class.

---
+++ include/Beard/ui/ProtoGrid.hpp
@@ -0,0 +1,589 @@
+/**
+@file ui/ProtoGrid.hpp
+@brief Prototype grid widget.
+
+@author Tim Howard
+@copyright 2014 Tim Howard under the MIT license;
+see @ref index or the accompanying LICENSE file for full text.
+*/
+
+#ifndef BEARD_UI_PROTOGRID_HPP_
+#define BEARD_UI_PROTOGRID_HPP_
+
+#include <Beard/config.hpp>
+#include <Beard/geometry.hpp>
+#include <Beard/ui/Defs.hpp>
+#include <Beard/ui/Widget/Defs.hpp>
+#include <Beard/ui/Widget/Base.hpp>
+
+#include <utility>
+
+namespace Beard {
+namespace ui {
+
+// Forward declarations
+struct GridView;
+struct GridRenderData;
+class ProtoGrid;
+
+/**
+	@addtogroup ui
+	@{
+*/
+
+// FIXME: Geh, type alias dependency (see sketch note)
+/**
+	Grid view.
+*/
+struct GridView final {
+	/** Entire frame of the view. */
+	Rect frame{{0, 0}, {0, 0}};
+	/** Header frame. */
+	Rect header_frame{{0, 0}, {0, 0}};
+	/** Content frame (not including header). */
+	Rect content_frame{{0, 0}, {0, 0}};
+	/** Number of rows that fit in the content frame. */
+	geom_value_type fit_count{0};
+	/** Number of columns in the column range. */
+	geom_value_type col_count{0};
+	/** Number of rows in the row range. */
+	geom_value_type row_count{0};
+	/** Column range. */
+	Vec2 col_range{0, 0};
+	/** Row range (bound to fit_count). */
+	Vec2 row_range{0, 0};
+};
+
+/**
+	Grid render data.
+*/
+struct GridRenderData final {
+	/** %Base render data. */
+	ui::Widget::RenderData& rd;
+
+	/**
+		Cached properties.
+		@{
+	*/
+	tty::attr_type
+		primary_fg{tty::Color::term_default},
+		primary_bg{tty::Color::term_default},
+		content_fg{tty::Color::term_default},
+		content_bg{tty::Color::term_default},
+		selected_fg{tty::Color::term_default},
+		selected_bg{tty::Color::term_default}
+	;
+	/** @} */
+
+	/**
+		Constructor with render data and cache.
+
+		@returns Return value.
+		@param rd %Base render data.
+		@param primary_active Whether to fetch active or inactive
+		primary attributes.
+		@param content_active Whether to fetch active or inactive
+		content attributes.
+	*/
+	GridRenderData(
+		ui::Widget::RenderData& rd,
+		bool const primary_active,
+		bool const content_active
+	) noexcept
+		: rd(rd)
+		, primary_fg(rd.get_attr(
+			primary_active
+			? ui::property_primary_fg_active
+			: ui::property_primary_fg_inactive
+		))
+		, primary_bg(rd.get_attr(
+			primary_active
+			? ui::property_primary_bg_active
+			: ui::property_primary_bg_inactive
+		))
+		, content_fg(rd.get_attr(
+			content_active
+			? ui::property_content_fg_active
+			: ui::property_content_fg_inactive
+		))
+		, content_bg(rd.get_attr(
+			content_active
+			? ui::property_content_bg_active
+			: ui::property_content_bg_inactive
+		))
+		, selected_fg(rd.get_attr(ui::property_content_fg_selected))
+		, selected_bg(rd.get_attr(ui::property_content_bg_selected))
+	{}
+};
+
+/**
+	Prototype grid widget.
+*/
+class ProtoGrid
+	: public ui::Widget::Base
+{
+private:
+	using base_type = ui::Widget::Base;
+
+public:
+	/** Index type. */
+	using index_type = geom_value_type;
+
+	/** Content actions. */
+	enum class ContentAction : unsigned {
+		select = 0u,
+		unselect,
+		select_toggle,
+		insert_before,
+		insert_after,
+		erase,
+		erase_selected
+	};
+
+protected:
+	/** Vector of ranges. */
+	using range_vector_type = aux::vector<Vec2>;
+
+private:
+	bool m_header_enabled{true};
+	bool m_header_enabled_next{true};
+	ui::ProtoGrid::index_type m_col_count{0};
+	ui::ProtoGrid::index_type m_row_count{0};
+
+	struct {
+		Vec2 header{0, 0};
+		range_vector_type rows{};
+	} m_dirty{};
+
+	ui::GridView m_view{};
+
+	aux::vector<geom_value_type> m_col_pos;
+
+	ProtoGrid() noexcept = delete;
+	ProtoGrid(ProtoGrid const&) = delete;
+	ProtoGrid& operator=(ProtoGrid const&) = delete;
+
+protected:
+/** @name Internals */ /// @{
+	/**
+		Set number of columns.
+
+		@param col_count New count.
+	*/
+	void
+	set_col_count(
+		ui::ProtoGrid::index_type const col_count
+	) noexcept {
+		m_col_count = max_ce(0, col_count);
+	}
+
+	/**
+		Set number of rows.
+
+		@param row_count New count.
+	*/
+	void
+	set_row_count(
+		ui::ProtoGrid::index_type const row_count
+	) noexcept {
+		m_row_count = max_ce(0, row_count);
+	}
+
+	/**
+		Get view (mutable).
+	*/
+	GridView&
+	get_view() noexcept {
+		return m_view;
+	}
+
+	/**
+		Enable or disable header.
+
+		@note is_header_enabled() will not report the new value until
+		reflow_view() is called.
+
+		@param header_enabled Whether to enable or disable the header.
+	*/
+	void
+	set_header_enabled(
+		bool const header_enabled
+	) noexcept {
+		if (header_enabled != m_header_enabled) {
+			m_header_enabled_next = header_enabled;
+		}
+	}
+
+	/**
+		Update the view range.
+
+		@note If @a retain_intersection is @c false, the cell render
+		queue will not be modified.
+
+		@param row_begin Start of row range.
+		@param row_end End of row range (non-inclusive).
+		@param col_begin Start of column range.
+		@param retain_intersection Whether to retain the queued
+		intersection with the previous range.
+	*/
+	void
+	update_view(
+		ui::ProtoGrid::index_type row_begin,
+		ui::ProtoGrid::index_type row_end,
+		ui::ProtoGrid::index_type col_begin,
+		bool const retain_intersection
+	) noexcept;
+
+	/**
+		Perform internal content action.
+
+		This will queue cell renders and shift the view if necessary.
+
+		This only responds to the @c insert_before, @c insert_after,
+		and @c erase actions. @c erase_selected should be decomposed
+		to @c erase actions by content_action(). The row count will
+		be updated.
+
+		@note The first three parameters are the same as for
+		content_action(). 
+
+		@param action Content action.
+		@param row_begin Start of row range.
+		@param count Number of rows.
+	*/
+	void
+	content_action_internal(
+		ui::ProtoGrid::ContentAction const action,
+		ui::ProtoGrid::index_type row_begin,
+		ui::ProtoGrid::index_type count
+	) noexcept;
+
+	/**
+		Queue header render.
+
+		@note If @a col_begin is @c -1, the column range <em>at point
+		of render</em> will be rendered.
+
+		@param col_begin Start of column range.
+		@param col_end End of column range (non-inclusive).
+	*/
+	void
+	queue_header_render(
+		ui::ProtoGrid::index_type col_begin = -1,
+		ui::ProtoGrid::index_type col_end = -1
+	) noexcept;
+
+	/**
+		Queue cell render.
+
+		@note If @a col_begin is @c -1, the column range <em>at point
+		of render</em> will be rendered.
+
+		@param row_begin Start of row range.
+		@param row_end End of row range (non-inclusive).
+		@param col_begin Start of column range.
+		@param col_end End of column range (non-inclusive).
+	*/
+	void
+	queue_cell_render(
+		ui::ProtoGrid::index_type row_begin,
+		ui::ProtoGrid::index_type row_end,
+		ui::ProtoGrid::index_type col_begin = -1,
+		ui::ProtoGrid::index_type col_end = -1
+	) noexcept;
+
+	/**
+		Reflow view.
+
+		@note This queues a cell render for the entire view range.
+
+		@param view_frame Area available to the grid view (header +
+		content).
+	*/
+	void
+	reflow_view(
+		Rect const& view_frame
+	) noexcept;
+
+	/**
+		Render the view.
+
+		@note This calls render_cells(). If the header is enabled,
+		render_header() is also called.
+
+		@param grid_rd Render data.
+		@param all Whether to render all cells or to only render
+		queued cells.
+	*/
+	void
+	render_view(
+		ui::GridRenderData& grid_rd,
+		bool const all
+	) noexcept;
+/// @}
+
+/** @name Implementation */ /// @{
+	/**
+		Perform content action.
+
+		@note @a row_end will be a count if an insertion action is
+		requested.
+
+		@param action Content action.
+		@param row_begin Start of row range.
+		@param count Number of rows.
+	*/
+	virtual void
+	content_action(
+		ui::ProtoGrid::ContentAction action,
+		ui::ProtoGrid::index_type row_begin,
+		ui::ProtoGrid::index_type count
+	) noexcept = 0;
+
+	/**
+		Render header cells.
+
+		@note Indices and ranges will always be within the bounds of
+		the current dimensions.
+
+		@param grid_rd Render data.
+		@param col_begin Start of column range.
+		@param col_end End of column range (non-inclusive).
+		@param frame Frame of the entire header row.
+	*/
+	virtual void
+	render_header(
+		ui::GridRenderData& grid_rd,
+		ui::ProtoGrid::index_type const col_begin,
+		ui::ProtoGrid::index_type const col_end,
+		Rect const& frame
+	) noexcept = 0;
+
+	/**
+		Render content cells.
+
+		@note Indices and ranges will always be within the bounds of
+		the current dimensions.
+
+		@param grid_rd Render data.
+		@param row Row to render.
+		@param col_begin Start of column range.
+		@param col_end End of column range (non-inclusive).
+		@param frame Frame of the entire visible row.
+	*/
+	virtual void
+	render_content(
+		ui::GridRenderData& grid_rd,
+		ui::ProtoGrid::index_type const row,
+		ui::ProtoGrid::index_type const col_begin,
+		ui::ProtoGrid::index_type const col_end,
+		Rect const& frame
+	) noexcept = 0;
+/// @}
+
+protected:
+/** @name Constructors and destructor */ /// @{
+	/** Destructor. */
+	virtual
+	~ProtoGrid() noexcept override = 0;
+
+	/**
+		Constructor.
+
+		@param root %Root.
+		@param flags Flags.
+		@param group %Property group.
+		@param geometry Geometry.
+		@param parent Parent.
+		@param col_count Number of columns.
+		@param row_count Number of rows.
+	*/
+	ProtoGrid(
+		ui::RootWPtr&& root,
+		ui::Widget::Flags const flags,
+		ui::group_hash_type const group,
+		ui::Geom&& geometry,
+		ui::Widget::WPtr&& parent,
+
+		ui::ProtoGrid::index_type const col_count,
+		ui::ProtoGrid::index_type const row_count
+	) noexcept
+		: base_type(
+			std::move(root),
+			flags,
+			group,
+			std::move(geometry),
+			std::move(parent)
+		)
+		, m_col_count(max_ce(ui::ProtoGrid::index_type{0}, col_count))
+		, m_row_count(max_ce(ui::ProtoGrid::index_type{0}, row_count))
+		, m_col_pos(unsigned_cast(m_col_count))
+	{}
+
+	/** Move constructor. */
+	ProtoGrid(ProtoGrid&&) = default;
+/// @}
+
+/** @name Operators */ /// @{
+	/** Move assignment operator. */
+	ProtoGrid& operator=(ProtoGrid&&) = default;
+/// @}
+
+public:
+/** @name Properties */ /// @{
+	/**
+		Check if the header is enabled.
+
+		@sa set_header_enabled()
+	*/
+	bool
+	is_header_enabled() const noexcept {
+		return m_header_enabled;
+	}
+
+	/**
+		Get the number of columns.
+	*/
+	ui::ProtoGrid::index_type
+	get_col_count() const noexcept {
+		return m_col_count;
+	}
+
+	/**
+		Get the number of rows.
+	*/
+	ui::ProtoGrid::index_type
+	get_row_count() const noexcept {
+		return m_row_count;
+	}
+
+	/**
+		Get view.
+	*/
+	GridView const&
+	get_view() const noexcept {
+		return m_view;
+	}
+/// @}
+
+/** @name Content */ /// @{
+	/**
+		Unselect all rows.
+	*/
+	void
+	select_none() noexcept {
+		content_action(ContentAction::unselect, 0, get_row_count());
+	}
+
+	/**
+		Select all rows.
+	*/
+	void
+	select_all() noexcept {
+		content_action(ContentAction::select, 0, get_row_count());
+	}
+
+	/**
+		Select or unselect a row range.
+
+		@param select Whether to select or unselect the range.
+		@param row_begin Start of range.
+		@param count Number of rows to select.
+	*/
+	void
+	select(
+		bool const select,
+		ui::ProtoGrid::index_type row_begin,
+		ui::ProtoGrid::index_type const count
+	) noexcept {
+		row_begin = value_clamp(row_begin, 0, get_row_count());
+		content_action(
+			select
+				? ContentAction::select
+				: ContentAction::unselect
+			,
+			row_begin,
+			max_ce(0, count)
+		);
+	}
+
+	/**
+		Flip the selection of a row range.
+
+		@param row_begin Start of range.
+		@param count Number of rows to flip.
+	*/
+	void
+	select_toggle(
+		ui::ProtoGrid::index_type row_begin,
+		ui::ProtoGrid::index_type const count
+	) noexcept {
+		row_begin = value_clamp(row_begin, 0, get_row_count());
+		content_action(ContentAction::select_toggle, row_begin, max_ce(0, count));
+	}
+
+	/**
+		Insert rows before a position.
+
+		@note Inserts @a count rows at @a row_begin.
+
+		@param row_begin Position to insert at.
+		@param count Number of rows to insert.
+	*/
+	void
+	insert_before(
+		ui::ProtoGrid::index_type row_begin,
+		ui::ProtoGrid::index_type const count
+	) noexcept {
+		row_begin = value_clamp(row_begin, 0, get_row_count());
+		content_action(ContentAction::insert_before, row_begin, max_ce(0, count));
+	}
+
+	/**
+		Insert rows after a position.
+
+		@note Inserts @a count rows at @a row_begin.
+
+		@param row_begin Position to insert at.
+		@param count Number of rows to insert.
+	*/
+	void
+	insert_after(
+		ui::ProtoGrid::index_type row_begin,
+		ui::ProtoGrid::index_type const count
+	) noexcept {
+		row_begin = value_clamp(row_begin, 0, get_row_count());
+		content_action(ContentAction::insert_after, row_begin, max_ce(0, count));
+	}
+
+	/**
+		Erase a row range.
+
+		@param row_begin Start of range.
+		@param count Number of rows to erase.
+	*/
+	void
+	erase(
+		ui::ProtoGrid::index_type row_begin,
+		ui::ProtoGrid::index_type const count
+	) noexcept {
+		row_begin = value_clamp(row_begin, 0, get_row_count());
+		content_action(ContentAction::erase, row_begin, max_ce(0, count));
+	}
+
+	/**
+		Erase all selected rows.
+	*/
+	void
+	erase_selected() noexcept {
+		content_action(ContentAction::erase_selected, 0, 0);
+	}
+/// @}
+};
+
+/** @} */ // end of doc-group ui
+
+} // namespace ui
+} // namespace Beard
+
+#endif // BEARD_UI_PROTOGRID_HPP_
---
+++ src/Beard/ui/ProtoGrid.cpp
@@ -0,0 +1,354 @@
+
+#include <Beard/ui/Defs.hpp>
+#include <Beard/ui/ProtoGrid.hpp>
+
+namespace Beard {
+namespace ui {
+
+// class ProtoGrid implementation
+
+ProtoGrid::~ProtoGrid() noexcept = default;
+
+// internals
+
+namespace {
+inline void
+expand_range(
+	Vec2& range,
+	ui::ProtoGrid::index_type const begin,
+	ui::ProtoGrid::index_type const end
+) noexcept {
+	if (-1 != range.x) {
+		if (range.x == range.y) {
+			range.x = begin;
+			range.y = end;
+		} else if (-1 == begin && begin == end) {
+			range.x = -1;
+			range.y = -1;
+		} else {
+			range.x = min_ce(range.x, begin);
+			range.y = max_ce(range.y, end);
+		}
+	}
+}
+} // anonymous namespace
+
+void
+ProtoGrid::update_view(
+	ui::ProtoGrid::index_type row_begin,
+	ui::ProtoGrid::index_type row_end,
+	ui::ProtoGrid::index_type col_begin,
+	bool const retain_intersection
+) noexcept {
+	row_begin = value_clamp(row_begin, 0, m_row_count);
+	row_end = min_ce(
+		value_clamp(row_end, row_begin, m_row_count),
+		row_begin + m_view.fit_count
+	);
+	col_begin = value_clamp(col_begin, 0, m_col_count);
+
+	Vec2 const nrr{row_begin, row_end};
+	auto const orc = m_view.row_count;
+	Vec2 const orr = m_view.row_range;
+
+	// TODO: Calculate actual col_range by frame and m_col_pos
+	m_view.col_count = m_col_count - col_begin;
+	m_view.row_count = row_end - row_begin;
+	m_view.col_range = {col_begin, m_col_count};
+	m_view.row_range = nrr;
+
+	/*DUCT_DEBUGF(
+		"update_view(): fit_count = %3d, row_count = %3d, col_count = %3d,\n"
+		"  row_range = {%3d, %3d}, col_range = {%3d, %3d}"
+		", orc = %3d"
+		", dirty.rows.size = %zu",
+		m_view.fit_count, m_view.row_count, m_view.col_count,
+		m_view.row_range.x, m_view.row_range.y,
+		m_view.col_range.x, m_view.col_range.y,
+		orc, m_dirty.rows.size()
+	);*/
+
+	if (retain_intersection) {
+		Vec2 const v_refresh{-1, -1};
+		switch (range_rel(nrr, orr)) {
+		case RangeRel::equal:
+			// Nothing to do
+			break;
+
+		case RangeRel::disjoint_before:
+		case RangeRel::disjoint_after:
+			std::fill(
+				m_dirty.rows.begin(),
+				m_dirty.rows.end(),
+				v_refresh
+			);
+			break;
+
+		case RangeRel::subset:
+		case RangeRel::intersection_after:
+			// fall-through
+
+		case RangeRel::superset:
+		case RangeRel::intersection_before:
+			auto const head = m_dirty.rows.begin();
+			if (nrr.x > orr.x) {
+				auto const idx = nrr.x - orr.x;
+				auto const amt = min_ce(m_view.row_count, orc - idx);
+				std::copy(head + idx, head + idx + amt, head);
+				std::fill(head + idx + amt, m_dirty.rows.end(), v_refresh);
+			} else if (nrr.x < orr.x) {
+				auto const idx = orr.x - nrr.x;
+				auto const amt = min_ce(orc, m_view.row_count - idx);
+				std::copy_backward(head, head + amt, head + idx + amt);
+				std::fill(head, head + amt, v_refresh);
+				std::fill(head + idx + amt, m_dirty.rows.end(), v_refresh);
+			} else {
+				std::fill(
+					m_dirty.rows.begin() + m_view.row_count,
+					m_dirty.rows.end(),
+					v_refresh
+				);
+			}
+			break;
+		}
+	}
+}
+
+void
+ProtoGrid::content_action_internal(
+	ui::ProtoGrid::ContentAction const action,
+	ui::ProtoGrid::index_type row_begin,
+	ui::ProtoGrid::index_type count
+) noexcept {
+	if (0 >= count) {
+		return;
+	}
+
+	row_begin = max_ce(0, row_begin);
+	switch (action) {
+	case ContentAction::insert_after:
+		// Cast insert_after in terms of insert_before
+		++row_begin;
+		// fall-through
+
+	case ContentAction::insert_before:
+	case ContentAction::erase:
+		// We only handle these three actions
+		break;
+
+	default:
+		// Nothing to do for other actions
+		return;
+	}
+
+	row_begin = min_ce(row_begin, m_row_count);
+	auto const row_end = min_ce(row_begin + count, m_row_count);
+
+	RangeRel const rel = range_rel(Vec2{row_begin, row_end}, m_view.row_range);
+	if (RangeRel::disjoint_after == rel) {
+		// disjoint_after does not affect view
+		return;
+	}
+
+	// For both erasure and insertion, queue everything from
+	// push_begin to the end of the view.
+	// Bound-checked for a tiny bit of optimization (don't need to
+	// mark anything outside the view itself)
+	auto const push_begin = max_ce(row_begin, m_view.row_range.x);
+	if (push_begin < m_view.row_range.y) {
+		std::fill(
+			m_dirty.rows.begin() + (push_begin - m_view.row_range.x),
+			m_dirty.rows.begin() + m_view.row_count,
+			Vec2{-1, -1}
+		);
+	}
+
+	if (ContentAction::erase == action) {
+		// Erasure is bound (row_end is bound to m_row_count)
+		set_row_count(m_row_count - (row_end - row_begin));
+		// TODO: Shift view up if it contains no rows
+	} else {
+		// Insertion is unbound
+		set_row_count(m_row_count + count);
+	}
+	update_view(
+		m_view.row_range.x,
+		m_view.row_range.x + m_view.fit_count,
+		m_view.col_range.x,
+		true
+	);
+}
+
+void
+ProtoGrid::queue_header_render(
+	ui::ProtoGrid::index_type col_begin,
+	ui::ProtoGrid::index_type col_end
+) noexcept {
+	if (is_header_enabled()) {
+		if (-1 == col_begin) {
+			col_end = -1;
+			m_dirty.header = {-1, -1};
+		} else {
+			col_begin = value_clamp(col_begin, m_view.col_range);
+			col_end = value_clamp(col_end, col_begin, m_view.col_range.y);
+			if (col_begin != col_end) {
+				expand_range(m_dirty.header, col_begin, col_end);
+			}
+		}
+	}
+}
+
+void
+ProtoGrid::queue_cell_render(
+	ui::ProtoGrid::index_type row_begin,
+	ui::ProtoGrid::index_type row_end,
+	ui::ProtoGrid::index_type col_begin,
+	ui::ProtoGrid::index_type col_end
+) noexcept {
+	// NB: row_begin will be clamped to the view range, which is
+	// already clamped to the number of rows. Also, row_begin needn't
+	// be clamped to m_row_count because row_end is clamped to that,
+	// and the bound comparison is row_end > row.
+	if (-1 == col_begin) {
+		col_end = -1;
+	} else {
+		col_begin = value_clamp(col_begin, m_view.col_range);
+		col_end = value_clamp(col_end, col_begin, m_view.col_range.y);
+		if (col_begin == col_end) {
+			return;
+		}
+	}
+
+	// Make row range relative to view
+	row_begin = value_clamp(row_begin - m_view.row_range.x, 0, m_row_count);
+	row_end = min_ce(
+		max_ce(
+			index_type{0},
+			min_ce(row_end, m_row_count) - m_view.row_range.x
+		),
+		m_view.row_count
+	);
+
+	for (
+		auto row = row_begin;
+		row_end > row;
+		++row
+	) {
+		expand_range(m_dirty.rows[row], col_begin, col_end);
+	}
+}
+
+void
+ProtoGrid::reflow_view(
+	Rect const& view_frame
+) noexcept {
+	if (
+		m_header_enabled == m_header_enabled_next &&
+		view_frame == m_view.frame
+	) {
+		return;
+	}
+
+	m_header_enabled = m_header_enabled_next;
+
+	m_view.frame = view_frame;
+	vec2_clamp_min(m_view.frame.size, Vec2{0, 0});
+	m_view.content_frame = m_view.frame;
+	if (is_header_enabled()) {
+		// Clamps here are to avoid sneaking outside the view frame
+		m_view.header_frame = m_view.frame;
+		m_view.header_frame.size.height = min_ce(
+			1,
+			m_view.frame.size.height
+		);
+
+		m_view.content_frame.pos.y = min_ce(
+			m_view.content_frame.pos.y + 1,
+			m_view.frame.pos.y + m_view.frame.size.height
+		);
+		m_view.content_frame.size.height = max_ce(
+			0,
+			m_view.content_frame.size.height - 1
+		);
+	} else {
+		m_view.header_frame = {{0, 0}, {0, 0}};
+	}
+
+	// We want to retain the row range at its beginning, so
+	// instead of calculating the intersection with the previous
+	// content frame, we're just queuing the entire range
+	m_view.fit_count = m_view.content_frame.size.height;
+	m_dirty.rows.resize(m_view.fit_count);
+	update_view(
+		m_view.row_range.x,
+		m_view.row_range.x + m_view.fit_count,
+		m_view.col_range.x,
+		false
+	);
+
+	if (is_header_enabled()) {
+		m_dirty.header = {-1, -1};
+	}
+	std::fill(
+		m_dirty.rows.begin(),
+		m_dirty.rows.end(),
+		Vec2{-1, -1}
+	);
+}
+
+void
+ProtoGrid::render_view(
+	ui::GridRenderData& grid_rd,
+	bool const all
+) noexcept {
+	Vec2 cr;
+	if (is_header_enabled()) {
+		cr = m_dirty.header;
+		if (-1 == cr.x) {
+			cr = m_view.col_range;
+		} else {
+			cr.x = value_clamp(cr.x, m_view.col_range);
+			cr.y = value_clamp(cr.y, cr.x, m_view.col_range.y);
+		}
+		if (all || cr.x < cr.y) {
+			render_header(grid_rd, cr.x, cr.y, m_view.header_frame);
+		}
+	}
+
+	auto it = m_dirty.rows.begin();
+	auto row = m_view.row_range.x;
+	Rect row_frame = m_view.content_frame;
+	row_frame.size.height = 1;
+	if (0 < range_length(m_view.col_range)) {
+		cr = m_view.col_range;
+		while (m_dirty.rows.end() != it) {
+			if (!all) {
+				cr = *it;
+				if (-1 == cr.x) {
+					cr = m_view.col_range;
+				} else {
+					cr.x = value_clamp(cr.x, m_view.col_range);
+					cr.y = value_clamp(cr.y, cr.x, m_view.col_range.y);
+				}
+			}
+			if (row >= m_view.row_range.y) {
+				break;
+			} else if (all || cr.x < cr.y) {
+				render_content(grid_rd, row, cr.x, cr.y, row_frame);
+				*it = {0, 0};
+			}
+			++it;
+			++row;
+			++row_frame.pos.y;
+		}
+	}
+
+	if (m_dirty.rows.end() != it) {
+		// FIXME: Destructive clear
+		row_frame.size.height = m_view.fit_count - m_view.row_count;
+		grid_rd.rd.terminal.clear_back(row_frame);
+	}
+}
+
+} // namespace ui
+} // namespace Beard
