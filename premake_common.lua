4cdceb816e17cd72d4400ffcc521e9a946689c30
Coranna Howard <me@komiga.com>
2014-07-20 20:46:35 -0400

    test/premake: added .elf suffix for Linux binaries; tidy.

--- premake_common.lua
+++ premake_common.lua
@@ -25,35 +25,6 @@ function action_clean()
 	end
 end
 
-function make_test(group, name, srcglob, configs)
-	precore.make_project(
-		group .. "_" .. name,
-		"C++", "ConsoleApp",
-		"./", "out/",
-		nil, configs
-	)
-
-	if nil == configs then
-		precore.apply("beard-strict")
-	end
-
-	if nil == srcglob then
-		srcglob = name .. ".cpp"
-	end
-
-	configuration {}
-		targetname(name)
-		files {
-			srcglob
-		}
-end
-
-function make_tests(group, tests)
-	for name, test in pairs(tests) do
-		make_test(group, name, test[0], test[1])
-	end
-end
-
 -- Custom precore configs
 
 precore.make_config(
--- test/premake4.lua
+++ test/premake4.lua
@@ -1,6 +1,38 @@
 
 dofile("../premake_common.lua")
 
+function make_test(group, name, srcglob, configs)
+	precore.make_project(
+		group .. "_" .. name,
+		"C++", "ConsoleApp",
+		"./", "out/",
+		nil, configs
+	)
+
+	if nil == configs then
+		precore.apply("beard-strict")
+	end
+
+	if nil == srcglob then
+		srcglob = name .. ".cpp"
+	end
+
+	configuration {}
+		targetname(name)
+		files {
+			srcglob
+		}
+
+	configuration {"linux"}
+		targetsuffix(".elf")
+end
+
+function make_tests(group, tests)
+	for name, test in pairs(tests) do
+		make_test(group, name, test[0], test[1])
+	end
+end
+
 -- Test solution
 
 precore.make_solution(
